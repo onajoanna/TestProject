c55eed5016e96190f51ec6dd85ab450f2e0a2ad6
onajoanna <90551644+onajoanna@users.noreply.github.com>
2021-09-12 11:51:08 +0200

    Initial Commit until combo controls
    Initial Commit until combo controls

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestProject
+{
+    public class Tests: DriverHelper
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+            driver = new ChromeDriver();
+         
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+
+            driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+
+            driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+       
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almond");
+         
+
+
+            Assert.Pass();
+        }
+    }
+}
